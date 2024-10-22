8b5fd7b36deb5f2b43ecf4503f78014661c89083
skalse <lavrishkovlad@gmail.com>
2024-10-22 22:14:26 +0000

    Fix no used generic

--- SaverData.cpp
+++ SaverData.cpp
@@ -4,6 +4,59 @@
 #include <nlohmann/json.hpp>
 using namespace std;
 
+
+void SaverData::Save(LibraryObject &libraryObject) {
+    string path = libraryObject.GetPath();
+    nlohmann::json jsonData;
+    int lastIndex = 0;
+
+    if (filesystem::path dir = filesystem::path(path).parent_path(); !exists(dir))
+        create_directories(dir);
+
+    ifstream inFile(path);
+    if (inFile.is_open()) {
+        try {
+            inFile >> jsonData;
+        } catch (const nlohmann::json::parse_error &e) {
+            cerr << "Error parsing JSON: " << e.what() << endl;
+            return;
+        }
+        inFile.close();
+
+        if (!jsonData.is_array()) {
+            cerr << "Invalid data format! A JSON array was expected!" << endl;
+            jsonData = nlohmann::json::array();
+        }
+
+        for (const auto &item : jsonData) {
+            if (item.contains("id") && item["id"].is_number_integer()) {
+                lastIndex = max(lastIndex, item["id"].get<int>());
+            }
+        }
+    } else {
+        jsonData = nlohmann::json::array();
+        ofstream outFile(path);
+        if (!outFile) {
+            cerr << "Error create file: " << path << endl;
+            return;
+        }
+        outFile << jsonData.dump(4);
+        outFile.close();
+        cout << "File succeed create: " << path << endl;
+    }
+    libraryObject.SetId(lastIndex + 1);
+    nlohmann::json json = libraryObject.GetSaveData();
+    jsonData.push_back(json);
+    ofstream outFile(path);
+    if (!outFile) {
+        cerr << "Error open write file: " << path << endl;
+        return;
+    }
+
+    outFile << jsonData.dump(4);
+    cout << "Data succeed save in file " << path << " with index " << lastIndex + 1 << endl;
+}
+
 void SaverData::Update(LibraryObject &object, int index) {
     ifstream inFile(object.GetPath());
     nlohmann::json jsonData;
--- SaverData.h
+++ SaverData.h
@@ -9,66 +9,11 @@
 using namespace std;
 class SaverData {
 public:
-    template<typename T>
-        typename enable_if<is_base_of<LibraryObject, T>::value>::type static Save(T &libraryObject);
+    static void Save(LibraryObject &libraryObject);
 
     static void Update(LibraryObject &object, int index);
 
     static void Delete(LibraryObject &object, int index);
 };
-
-template<typename T>
-typename enable_if<is_base_of<LibraryObject, T>::value>::type
-SaverData::Save(T &libraryObject) {
-    string path = libraryObject.GetPath();
-    nlohmann::json jsonData;
-    int lastIndex = 0;
-
-    if (filesystem::path dir = filesystem::path(path).parent_path(); !exists(dir))
-        create_directories(dir);
-
-    ifstream inFile(path);
-    if (inFile.is_open()) {
-        try {
-            inFile >> jsonData;
-        } catch (const nlohmann::json::parse_error &e) {
-            cerr << "Error parsing JSON: " << e.what() << endl;
-            return;
-        }
-        inFile.close();
-
-        if (!jsonData.is_array()) {
-            cerr << "Invalid data format! A JSON array was expected!" << endl;
-            jsonData = nlohmann::json::array();
-        }
-
-        for (const auto &item : jsonData) {
-            if (item.contains("id") && item["id"].is_number_integer()) {
-                lastIndex = max(lastIndex, item["id"].get<int>());
-            }
-        }
-    } else {
-        jsonData = nlohmann::json::array();
-        ofstream outFile(path);
-        if (!outFile) {
-            cerr << "Error create file: " << path << endl;
-            return;
-        }
-        outFile << jsonData.dump(4);
-        outFile.close();
-        cout << "File succeed create: " << path << endl;
-    }
-    libraryObject.SetId(lastIndex + 1);
-    nlohmann::json json = libraryObject.GetSaveData();
-    jsonData.push_back(json);
-    ofstream outFile(path);
-    if (!outFile) {
-        cerr << "Error open write file: " << path << endl;
-        return;
-    }
-
-    outFile << jsonData.dump(4);
-    cout << "Data succeed save in file " << path << " with index " << lastIndex + 1 << endl;
-}
 
 #endif //SAVERDATA_H
